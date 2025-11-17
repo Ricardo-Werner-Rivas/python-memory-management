0cf84b1d634db83a4e1ffee7422c5d8cbbd76977
Ricardo Werner Rivas <ricardowernerrivas@gmail.com>
2025-11-18 00:00:12 +0100

    Update comment legend

--- memorymanagement/pointers/core.py
+++ memorymanagement/pointers/core.py
@@ -1,8 +1,13 @@
+#*===============================================================================================================================
 #* LEGEND
 #! Missing
 #* Section
 #^ Important
+# Normal comment
 #// Alternative or deprecated code
+#*===============================================================================================================================
+
+#^ The different types of comments require the "Colorful Comments Refreshed" extension for VSCode to be properly distinguished
 
 #* IMPORTS
 # Import "TypeVar" and "Generic"
